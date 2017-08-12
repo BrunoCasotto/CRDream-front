4e59eb3be2b523a6593fb965523ad8fa32f5d898
Bruno casotto <casottoalves@hotmail.com>
2017-08-11 23:46:55 -0300

    reset sass done

--- webpack/assetsFilesConfig.js
+++ webpack/assetsFilesConfig.js
@@ -1,12 +1,15 @@
 const path = require('path');
 
-const views_path = path.resolve(__dirname, '..', 'views');
+const pages_path = path.join(__dirname, '..', 'views','pages')
 
-const pages_path = path.join(views_path,'pages')
+const sass_path = path.join(__dirname, '..', 'resources','sass')
 
 module.exports = {
+  bundle: [path.join(sass_path, 'reset.scss')],
+
   home: path.join(pages_path, 'home', 'script'),
   home: path.join(pages_path, 'home', 'style.scss'),
+
   contact: path.join(pages_path, 'contact', 'script'),
   contact: path.join(pages_path, 'contact', 'style.scss'),
 }
